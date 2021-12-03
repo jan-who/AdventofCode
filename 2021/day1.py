7a9a03c2972bbc34bc11f7050eef5c56bbb6b15b
Jan Wolter <jwolter@posteo.de>
2021-12-03 13:12:33 +0100

    Day 1 - Part 2

--- 2021/day1.py
+++ 2021/day1.py
@@ -15,7 +15,7 @@ testdata = """199
 263"""
 testdata = [x for x in testdata.splitlines()]
 
-def run(test=False):
+def part_one(test=False):
     if test:
         data = testdata
     else:
@@ -31,7 +31,34 @@ def run(test=False):
             counter += 1
         previous_no = n
 
-    print(f"Increases: {counter}")
+    print(f"Part 1: Increases: {counter}")
+
+def part_two(test=False):
+    if test:
+        data = testdata
+    else:
+        data = file
+
+    # convert data from str to int
+    data_int = [int(x) for x in data]
+
+    idx = 0
+    previous_sum = None
+    counter = 0
+    while True:
+        current_sum = sum(data_int[idx:(idx+3)])
+
+        if previous_sum != None and current_sum > previous_sum:
+            counter += 1
+
+        previous_sum = current_sum
+        idx += 1
+
+        if len(data_int) <= idx:
+            break
+
+    print(f"Part 2: Increases: {counter}")
 
 if __name__ == "__main__":
-    run()
+    part_one()
+    part_two()
